b24a491f6572e23fd09a80c6f0110afbfdd0b17c
Francisco Trivino <ftrivino@redhat.com>
2024-10-09 10:42:32 +0200

    Remove clients from ipacluster
    mrack brings client containers without --dns option. Hence af automation fails to install clients.
    switch to manual mode.
    
    Signed-off-by: Francisco Trivino <ftrivino@redhat.com>

--- generate-env.sh
+++ generate-env.sh
@@ -21,13 +21,28 @@ cd ..
 
 cp data/ansible.cfg .
 
+exit 0
+
 export ANSIBLE_CONFIG=$(pwd)/ansible.cfg
 
+# Install Server
 ansible-playbook -i data/mrack-inventory.yaml -i data/ipa1demo.hosts \
                  -c podman af/playbooks/install-cluster.yml
 
+# Install Client
+podman stop client1-ipa1demo-test-local-freeipa-test-test
+podman run --dns 10.89.0.4 -d --privileged --hostname client1.ipa1demo.test --name=client1-ipa1demo-test-local-freeipa-test-test --replace fedora-freeipa:40
+podman exec -it client1-ipa1demo-test-local-freeipa-test-test ipa-client-install --domain ipa1demo.test --realm IPA1DEMO.TEST -N --mkhomedir -p admin --password=Secret123 -U
+
+
+# Install Server
 ansible-playbook -i data/mrack-inventory.yaml -i data/ipa2demo.hosts \
                  -c podman af/playbooks/install-cluster.yml
+
+# Install Client
+podman stop client2-ipa2demo-test-local-freeipa-test-test
+podman run --dns 10.89.0.5 -d --privileged --hostname client2.ipa2demo.test --name=client2-ipa2demo-test-local-freeipa-test-test --replace fedora-freeipa:40
+podman exec -it client2-ipa2demo-test-local-freeipa-test-test ipa-client-install --domain ipa2demo.test --realm IPA2DEMO.TEST -N --mkhomedir -p admin --password=Secret123 -U
 
 ansible-playbook -i data/mrack-inventory.yaml \
                  -c podman data/establish-trust.yaml
